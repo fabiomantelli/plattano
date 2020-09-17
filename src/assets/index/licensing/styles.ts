5facb1c999ed8167217efb27590bfa4010c3ec75
Fabio Mantelli <fmmantelli@gmail.com>
2020-09-17 14:46:55 -0300

    refactor(icons left padding): added padding 2rem to licensing, reference and services icons

--- src/assets/index/licensing/styles.ts
+++ src/assets/index/licensing/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    padding-left: 2rem;
     @media only screen and (max-width: 61.98em) {
         display: flex;
         justify-content: center;
