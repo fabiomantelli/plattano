cad59df358fc556f2d72abb3a4824bccc8dbfff5
Fabio Mantelli <fmmantelli@gmail.com>
2020-09-17 15:20:25 -0300

    refactor(layout): icon layout

--- src/assets/index/where/styles.ts
+++ src/assets/index/where/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    padding-top: 2rem;
+    padding: 2rem 0 0 2rem;
 
     @media only screen and (max-width: 42.98em) {
         display: flex;
         justify-content: center;
+        padding: 2rem 0 0 0;
     }
 `
