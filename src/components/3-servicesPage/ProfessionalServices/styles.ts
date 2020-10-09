0cac83b6e0a94c5a0c18982b04353abf30b9a3ef
Fabio Mantelli <fmmantelli@gmail.com>
2020-10-09 14:19:33 -0300

    fix(styled2): added styled2

--- src/components/3-servicesPage/ProfessionalServices/styles.ts
+++ src/components/3-servicesPage/ProfessionalServices/styles.ts
@@ -1,3 +1,45 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+    /* height: calc(100vh - 70px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 2rem;
+    background-image: url('/assets/3-services/profissionalServices/profissionalservices-image.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: bottom;
+    h1 {
+        width: 60%;
+        font-size: 4rem;
+        font-weight: normal;
+        color: ${props =>
+        props.theme.colors
+            .background};
+    }
+    h2 {
+        width: 50%;
+        font-size: 1rem;
+        font-weight: 400;
+        color: ${props =>
+        props.theme.colors
+            .background};
+        line-height: 1.5rem;
+        padding-bottom: 80px;
+        padding-top: 30px;
+    }
+    @media only screen and (max-width: 61.98em) {
+        text-align: center;
+        background-image: none;
+
+        h1 {
+            width: 100%;
+        }
+
+        h2 {
+            padding-top: 1rem;
+            width: 100%;
+        }
+    } */
+`
