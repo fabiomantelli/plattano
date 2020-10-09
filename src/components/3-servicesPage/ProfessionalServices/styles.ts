260ae71d5688292abc9de5a966c6aa0cfe81e7a0
Fabio Mantelli <fmmantelli@gmail.com>
2020-10-09 15:38:51 -0300

    fix(test h1 and h2): test test h1 and h2

--- src/components/3-servicesPage/ProfessionalServices/styles.ts
+++ src/components/3-servicesPage/ProfessionalServices/styles.ts
@@ -11,19 +11,12 @@ export const Container = styled.div`
     background-size: cover;
     background-position: bottom;
 
-    .H1 {
+    h1 {
         font-size: 4rem;
-        font-weight: normal;
-        color: ${props => props.theme.colors.background};
     }
 
-    .H2 {
+    h2 {
         font-size: 1rem;
-        font-weight: 400;
-        color: ${props => props.theme.colors.background};
-        line-height: 1.5rem;
-        padding-bottom: 80px;
-        padding-top: 30px;
     }
 
     @media only screen and (max-width: 61.98em) {
