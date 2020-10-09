5d0cd088010718df91ef8dca05f1a79893042590
Fabio Mantelli <fmmantelli@gmail.com>
2020-10-09 15:13:39 -0300

    fix(services style test): test service

--- src/components/3-servicesPage/ProfessionalServices/styles.ts
+++ src/components/3-servicesPage/ProfessionalServices/styles.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    /* height: calc(100vh - 70px);
-    display: flex;
+    height: calc(100vh - 70px);
+    /*display: flex;
     flex-direction: column;
     justify-content: center;
     padding: 2rem;
