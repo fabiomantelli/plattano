74001e7ac7cf480e718a4e17f24ac2d404698bbe
Fabio Mantelli <fmmantelli@gmail.com>
2020-10-09 14:55:16 -0300

    fix(test first row): test

--- src/components/3-servicesPage/ProfessionalServices/styles.ts
+++ src/components/3-servicesPage/ProfessionalServices/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    height: calc(100vh - 70px);
+    /* height: calc(100vh - 70px);
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -13,12 +13,16 @@ export const Container = styled.div`
     h1 {
         font-size: 4rem;
         font-weight: normal;
-        color: ${props => props.theme.colors.background};
+        color: ${props =>
+        props.theme.colors
+            .background};
     }
     h2 {
         font-size: 1rem;
         font-weight: 400;
-        color: ${props => props.theme.colors.background};
+        color: ${props =>
+        props.theme.colors
+            .background};
         line-height: 1.5rem;
         padding-bottom: 80px;
         padding-top: 30px;
@@ -26,5 +30,5 @@ export const Container = styled.div`
     @media only screen and (max-width: 61.98em) {
         text-align: center;
         background-image: none;
-    }
+    } */
 `
