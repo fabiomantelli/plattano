56671d94554090b1577bdd140e5cf241c0b7f671
Fabio Mantelli <fmmantelli@gmail.com>
2020-10-09 21:54:55 -0300

    fix(logo height): logo height ok

--- src/assets/5-parners/veeamGreenIcon/veeamGreenIcon.tsx
+++ src/assets/5-parners/veeamGreenIcon/veeamGreenIcon.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
-import { Container } from './styles'
 function veeamGreenIcon(): JSX.Element {
     return (
-        <Container>
-            <img
-                src="/assets/5-partners/veeamGreen/veeamgreen-icon.png"
-                alt="Veeam Green Icon"
-            />
-        </Container>
+        <img
+            src="/assets/5-partners/veeamGreen/veeamgreen-icon.png"
+            alt="Veeam Green Icon"
+        />
     )
 }
 
--- src/assets/5-parners/vmwareBlackIcon/vmwareBlackIcon.tsx
+++ src/assets/5-parners/vmwareBlackIcon/vmwareBlackIcon.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
-import { Container } from './styles'
 function vmwareBlackIcon(): JSX.Element {
     return (
-        <Container>
-            <img
-                src="/assets/5-partners/vmwareBlack/vmwareblack-icon.png"
-                alt="VMware Black Icon"
-            />
-        </Container>
+        <img
+            src="/assets/5-partners/vmwareBlack/vmwareblack-icon.png"
+            alt="VMware Black Icon"
+        />
     )
 }
 
