898e57d3292461870cd0915646f948de5325f6e7
Fábio Mantelli <fabiomantelli@MacBook-Air-de-Macbook.local>
2021-01-11 15:28:46 -0300

    feat(aws): added aws into project

--- src/pages/knowledgecenter.tsx
+++ src/pages/knowledgecenter.tsx
@@ -27,7 +27,6 @@ import { ButtonElement } from '../styles/Buttons'
 import News from '../components/News'
 import Email from '../components/Email'
 import Footer from '../components/Footer'
-import Link from 'next/link'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
