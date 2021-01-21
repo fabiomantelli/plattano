ebac5304d451e8cf5a86b28d4ea212806e42f8f0
Fábio Mantelli <fabiomantelli@MacBook-Air-de-Macbook.local>
2021-01-21 18:38:45 -0300

    fix(gtm): added google tag manager to _document

--- _document.tsx
+++ _document.tsx
@@ -49,6 +49,26 @@ export default class MyDocument extends Document {
                     />
 
                     <link rel="icon" href="../../public/favicon.ico" />
+                    
+                    {/* Google Tag Manager */}
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                })(window,document,'script','dataLayer','GTM-P4CF9TP');`,
+                        }}
+                    />
+                    {/* End Google Tag Manager */}
+                    {/* Google Tag Manager (noscript) */}
+                    <noscript
+                        dangerouslySetInnerHTML={{
+                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4CF9TP" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+                        }}
+                    />
+                    {/* End Google Tag Manager (noscript) */}
+
                 </Head>
 
                 <body>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,25 +17,6 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
             src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"
         ></script>
 
-        {/* Google Tag Manager */}
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                    })(window,document,'script','dataLayer','GTM-P4CF9TP');`,
-            }}
-        />
-        {/* End Google Tag Manager */}
-        {/* Google Tag Manager (noscript) */}
-        <noscript
-            dangerouslySetInnerHTML={{
-                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P4CF9TP" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-        />
-        {/* End Google Tag Manager (noscript) */}
-
         </>
     )
 }
