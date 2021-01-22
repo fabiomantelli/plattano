8a63e32e047ee507fa7c87fdd596c252cdf8e3bf
Fábio Mantelli <fabiomantelli@MacBook-Air-de-Macbook.local>
2021-01-22 18:51:30 -0300

    fix(env): added env.local file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,17 +42,17 @@ export default class MyDocument extends Document {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', '${GTM_ID}');
+              })(window,document,'script','dataLayer','${GTM_ID}');
               `,
             }}
           />
         </Head>
         <body>
           <noscript>
-            <iframe
+            <iframe 
               src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
+              height="0" 
+              width="0" 
               style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
