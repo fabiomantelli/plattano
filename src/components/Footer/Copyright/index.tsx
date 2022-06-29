4834e008d667ffc1cc89cd3694704d1f25068a32
unknown <fmmantelli@gmail.com>
2022-06-29 12:17:05 -0300

    feat(date/footer): added automatic date to footer

--- src/components/Footer/Copyright/index.tsx
+++ src/components/Footer/Copyright/index.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 import { Container } from './styles'
 
 const End: React.FC = () => {
+    const current = new Date();
+    const date = `${current.getFullYear()}`;
+
     return (
         <Container>
-            <h1>2021 Plattano. Todos os direitos reservados.</h1>
+            <h1>{date} Plattano. Todos os direitos reservados.</h1>
         </Container>
     )
 }
