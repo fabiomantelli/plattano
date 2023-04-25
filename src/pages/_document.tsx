0ec9111364741e3a83b5590128ac7c6f78485d30
Fabio Mantelli <fmmantelli@gmail.com>
2023-04-25 08:20:06 -0300

    feat(gta): added Google Tag Manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 // import { GTM_ID } from '../lib/gtm'
-// import { GA_TRACKING_ID } from '../lib/gtag'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocuments extends Document {
 
@@ -37,55 +37,30 @@ export default class MyDocuments extends Document {
 
       <Html>
         <Head>
-
-
-
-          
           {/* Google Tag Manager - Global base code */}
-          {/* <script
+          <script
             dangerouslySetInnerHTML={{
               __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
+              })(window,document,'script','dataLayer','${GA_TRACKING_ID}');
               `,
             }}
-          /> */}
-
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          /> */}
-          {/* <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-          `
-            }}
-          /> */}
-
-
+          />
         </Head>
 
-        {/* <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/2ae0587c-f351-4f14-ab26-c60bc9574c0a-loader.js" ></script> */}
-
         <body>
-          {/* <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript> */}
+         {/* Add Google Tag Manager (noscript) */}
+         <noscript
+            dangerouslySetInnerHTML={{
+              __html: `
+                <iframe src="https://www.googletagmanager.com/ns.html?id=${GA_TRACKING_ID}"
+                height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              `,
+            }}
+          />
 
           <Main />
           <NextScript />
